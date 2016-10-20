9f8bcf484cdfcca92593d9fe6224d32416f9e534
russ <russvetsper@gmail.com>
2016-10-19 17:48:28 -0700

    add new task , edit task

---
+++ app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }   from './app.component';
+import { PiesListComponent } from './pies-list.component';
+import { TaskListComponent } from './task-list.component';
+import { EditTaskComponent } from './edit-task.component';
+import { NewTaskComponent } from './new-task.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    PiesListComponent,
+    TaskListComponent,
+    EditTaskComponent,
+    NewTaskComponent,
+  ],
+  bootstrap:    [ AppComponent ]
+})
+
+export class AppModule { }
